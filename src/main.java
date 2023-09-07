a9e52591bfdbf98b77c8c21023e7723f979496ea
Ekaterina <gaurung505@gmail.com>
2023-09-07 20:25:32 +0400

    main

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+
+        int ticket = 400;
+        int bonus = 20;
+        int miles = ticket / bonus;
+
+
+        System.out.println("Бесплатные мили:");
+        System.out.println(miles);
+    }
+}
